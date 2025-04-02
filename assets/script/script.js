e588ba3d3f06cce206113e0c61ca72c1936915f9
DrakoulisE <DrakoulisE@gmail.com>
2025-04-02 22:17:37 +0300

    changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,6 +16,21 @@ header_projects.addEventListener("click", () => {
     projects_target.scrollIntoView({behavior: "smooth"});
 })
 
+header_contact.addEventListener("click", ()=>{
+    contact_target.scrollIntoView({behavior: "smooth"});
+})
+
 home_button.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: "smooth"});
+})
+
+const github_Banner_Button = document.getElementById("github");
+github_Banner_Button.addEventListener("click", ()=>{
+    window.open('https://github.com/DrakoulisE', '_blank');
+})
+
+const project_gyrogyro_button = document.getElementById("project_gyrogyro");
+
+project_gyrogyro_button.addEventListener("click", ()=>{
+    window.open('https://drakoulise.github.io/gyrogyro/', '_blank');
 })
